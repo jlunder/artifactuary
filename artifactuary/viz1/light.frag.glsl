0c8f9f7bb732c4775cc5234cf09608267b900ac3
Joseph Lunderville <jlunder@catfish.dhs.org>
2013-08-10 22:55:33 +0000

    switch to setting colors via uniforms instead of as per vertex data

--- artifactuary/viz1/light.frag.glsl
+++ artifactuary/viz1/light.frag.glsl
@@ -1,10 +1,10 @@
 precision lowp float;
 
-varying vec4 v_color;
+uniform vec4 u_color;
+
 varying vec2 v_texCoord;
 
 void main(void)
 {
-	const vec2 middle = vec2(0.5, 0.5);
-	gl_FragColor = v_color * max(0.0, 1.0 - 2.0 * distance(middle, v_texCoord));
+	gl_FragColor = u_color * max(0.0, 1.0 - length(v_texCoord));
 }
--- artifactuary/viz1/main.c
+++ artifactuary/viz1/main.c
@@ -1,14 +1,5 @@
-/**
- * OpenGL ES 2.0 - Example 01
- *
- * @author	Norbert Nopper norbert@nopper.tv
- *
- * Homepage: http://nopper.tv
- *
- * Copyright Norbert Nopper
- */
-
 #include <stdio.h>
+#include <stdint.h>
 
 #include "GLES2/glus.h"
 
@@ -17,20 +8,34 @@ static GLUSshaderprogram g_program;
 
 static GLint g_viewProjectionMatrixLocation;
 static GLint g_modelMatrixLocation;
+static GLint g_colorLocation;
 
 static GLint g_vertexLocation;
-static GLint g_colorLocation;
-static GLint g_texCoordLocation;
 
 static GLuint g_verticesVBO;
-static GLuint g_colorsVBO;
-static GLuint g_texCoordsVBO;
 
 static GLfloat g_aspectRatio;
 
-/**
- * Function for initialization.
- */
+
+#define ARRAY_WIDTH 110
+#define ARRAY_HEIGHT 18
+
+
+uint8_t array_colors[ARRAY_HEIGHT][ARRAY_WIDTH][3];
+
+
+void process(void)
+{
+    for(int j = 0; j < ARRAY_HEIGHT; ++j) {
+        for(int i = 0; i < ARRAY_WIDTH; ++i) {
+            array_colors[j][i][0] = i * 256 / ARRAY_WIDTH;
+            array_colors[j][i][1] = j * 256 / ARRAY_HEIGHT;
+            array_colors[j][i][2] = rand() % 256;
+        }
+    }
+}
+
+
 GLUSboolean init(GLUSvoid)
 {
     GLUStextfile vertexSource;
@@ -39,21 +44,14 @@ GLUSboolean init(GLUSvoid)
     glusLoadTextFile("light.vert.glsl", &vertexSource);
     glusLoadTextFile("light.frag.glsl", &fragmentSource);
     glusBuildProgramFromSource(&g_program, (const GLUSchar**)&vertexSource.text, (const GLUSchar**)&fragmentSource.text);
-    //printf("Vertex shader:\n%s\n--------\n", vertexSource.text);
-    //printf("Fragment shader:\n%s\n--------\n", fragmentSource.text);
     glusDestroyTextFile(&vertexSource);
     glusDestroyTextFile(&fragmentSource);
     
     g_viewProjectionMatrixLocation = glGetUniformLocation(g_program.program, "u_viewProjectionMatrix");
     g_modelMatrixLocation = glGetUniformLocation(g_program.program, "u_modelMatrix");
-    
-    //printf("uniforms: %d %d\n", g_viewProjectionMatrixLocation, g_modelMatrixLocation);
+    g_colorLocation = glGetUniformLocation(g_program.program, "u_color");
     
     g_vertexLocation = glGetAttribLocation(g_program.program, "a_vertex");
-    g_colorLocation = glGetAttribLocation(g_program.program, "a_color");
-    g_texCoordLocation = glGetAttribLocation(g_program.program, "a_texCoord");
-    
-    //printf("attributes: %d %d %d\n", g_vertexLocation, g_colorLocation, g_texCoordLocation);
     
     //glEnable(GL_DEPTH_TEST);
     
@@ -62,7 +60,7 @@ GLUSboolean init(GLUSvoid)
     glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
     
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-    glClearDepthf(1.0f);
+    //glClearDepthf(1.0f);
     
     GLfloat lightVertices[] = {
         -1.0f,  1.0f, 0.0f, 1.0f,
@@ -71,43 +69,16 @@ GLUSboolean init(GLUSvoid)
          1.0f, -1.0f, 0.0f, 1.0f,
     };
     
-    GLfloat lightColors[] = {
-        1.0f, 1.0f, 1.0f, 0.0f,
-        1.0f, 1.0f, 1.0f, 0.0f,
-        1.0f, 1.0f, 1.0f, 0.0f,
-        1.0f, 1.0f, 1.0f, 0.0f,
-    };
-    
-    GLfloat lightTexCoords[] = {
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        0.0f, 1.0f,
-        1.0f, 1.0f,
-    };
-    
     glGenBuffers(1, &g_verticesVBO);
     glBindBuffer(GL_ARRAY_BUFFER, g_verticesVBO);
     glBufferData(GL_ARRAY_BUFFER, 4 * 4 * sizeof (GLfloat), (GLfloat*)lightVertices, GL_STATIC_DRAW);
-    
-    glGenBuffers(1, &g_colorsVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, g_colorsVBO);
-    glBufferData(GL_ARRAY_BUFFER, 4 * 4 * sizeof (GLfloat), (GLfloat*)lightColors, GL_STATIC_DRAW);
-    
-    glGenBuffers(1, &g_texCoordsVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, g_texCoordsVBO);
-    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof (GLfloat), (GLfloat*)lightTexCoords, GL_STATIC_DRAW);
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     
     return GLUS_TRUE;
 }
 
-/**
- * Function is called before first update and every time when the window is resized.
- *
- * @param w	width of the window
- * @param h	height of the window
- */
+
 GLUSvoid reshape(GLUSint width, GLUSint height)
 {
     // Set the viewport depending on the width and height of the window.
@@ -116,18 +87,19 @@ GLUSvoid reshape(GLUSint width, GLUSint height)
     g_aspectRatio = (GLfloat)width / (GLfloat)height;
 }
 
-/**
- * Function to render and display content. Swapping of the buffers is automatically done.
- *
- * @return true for continuing, false to exit the application
- */
+
 GLUSboolean update(GLUSfloat time)
 {
     GLfloat viewMatrix[16];
     GLfloat viewProjectionMatrix[16];
     GLfloat modelMatrix[16];
     
+    
+    process();
+    
+    
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
     
     glusLookAtf(viewMatrix,
         0.0f, 0.0f, 8.0f,
@@ -136,68 +108,58 @@ GLUSboolean update(GLUSfloat time)
     glusPerspectivef(viewProjectionMatrix, 45.0f, g_aspectRatio, 0.1f, 1000.0f);
     glusMatrix4x4Multiplyf(viewProjectionMatrix, viewProjectionMatrix, viewMatrix);
     
+    
     glUseProgram(g_program.program);
     
-    glUniformMatrix4fv(g_viewProjectionMatrixLocation, 1, GL_FALSE, viewProjectionMatrix);
     
-    glusMatrix4x4Identityf(modelMatrix);
-    glUniformMatrix4fv(g_modelMatrixLocation, 1, GL_FALSE, modelMatrix);
+    glUniformMatrix4fv(g_viewProjectionMatrixLocation, 1, GL_FALSE, viewProjectionMatrix);
     
     glBindBuffer(GL_ARRAY_BUFFER, g_verticesVBO);
     glVertexAttribPointer(g_vertexLocation, 4, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(g_vertexLocation);
     
-    glBindBuffer(GL_ARRAY_BUFFER, g_colorsVBO);
-    glVertexAttribPointer(g_colorLocation, 4, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(g_colorLocation);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, g_texCoordsVBO);
-    glVertexAttribPointer(g_texCoordLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(g_texCoordLocation);
-    
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     
+    //
+    glusMatrix4x4Identityf(modelMatrix);
+    glUniformMatrix4fv(g_modelMatrixLocation, 1, GL_FALSE, modelMatrix);
+    
+    glUniform4f(g_colorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+    
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    //
     
     glDisableVertexAttribArray(g_vertexLocation);
-    glDisableVertexAttribArray(g_colorLocation);
-    glDisableVertexAttribArray(g_texCoordLocation);
     
     glUseProgram(0);
     
     return GLUS_TRUE;
 }
 
-/**
- * Function to clean up things.
- */
+
 GLUSvoid terminate(GLUSvoid)
 {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     
     glDeleteBuffers(1, &g_verticesVBO);
-    glDeleteBuffers(1, &g_colorsVBO);
-    glDeleteBuffers(1, &g_texCoordsVBO);
     
     glUseProgram(0);
     
     glusDestroyProgram(&g_program);
 }
 
-/**
- * Main entry point.
- */
+
 int main(int argc, char* argv[])
 {
     EGLint eglAttributes[] = {
-        EGL_RED_SIZE, 8,
-        EGL_GREEN_SIZE, 8,
-        EGL_BLUE_SIZE, 8,
-        EGL_DEPTH_SIZE, 16,
+        EGL_RED_SIZE, 5,
+        EGL_GREEN_SIZE, 6,
+        EGL_BLUE_SIZE, 5,
         EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
         EGL_NONE
     };
-
+    
+    
     glusInitFunc(init);
 
     glusReshapeFunc(reshape);
